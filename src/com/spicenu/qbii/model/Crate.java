8847cad4d6f726c2b56976675689e5252a9c5d15
aclin <devilsodo@gmail.com>
2014-01-19 20:09:51 -0800

    Level layout loaded by external file, read by loadLevel() in Crate.java

--- src/com/spicenu/qbii/model/Crate.java
+++ src/com/spicenu/qbii/model/Crate.java
@@ -1,8 +1,12 @@
 package com.spicenu.qbii.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 public class Crate {
@@ -13,14 +17,41 @@ public class Crate {
 	
 	public Crate() {
 		createDemo();
+		loadLevel();
 	}
 	
 	private void createDemo() {
 		this.jo = new Jo(new Vector2(-1, 5));
-		this.wall = new Wall(new Vector2(8, 4.75f), Wall.State.OPAQUE);
-		walls.add(new Wall(new Vector2(3, 4.75f), Wall.State.OPAQUE));
-		walls.add(new Wall(new Vector2(6, 4.75f), Wall.State.CLEAR));
-		walls.add(new Wall(new Vector2(9, 4.75f), Wall.State.OPAQUE));
+//		this.wall = new Wall(new Vector2(8, 4.75f), Wall.State.OPAQUE);
+//		walls.add(new Wall(new Vector2(3, 4.75f), Wall.State.OPAQUE));
+//		walls.add(new Wall(new Vector2(6, 4.75f), Wall.State.CLEAR));
+//		walls.add(new Wall(new Vector2(9, 4.75f), Wall.State.OPAQUE));
+	}
+	
+	private void loadLevel() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(Gdx.files.internal("levels/lvl1").reader());
+			String line;
+			while ((line = br.readLine()) != null) {
+				Gdx.app.log("Crate", line);
+				char key = line.charAt(0);
+				String[] strs;
+				switch (key) {
+				case 'W':
+					strs = line.split(" ");
+					walls.add(new Wall(new Vector2(Float.parseFloat(strs[1]), Float.parseFloat(strs[2])),
+										Integer.parseInt(strs[3]) == 0 ? Wall.State.CLEAR : Wall.State.OPAQUE));
+					break;
+				case 'T':
+					break;
+				case '%':
+					break;
+				}
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 	
 	public Jo getJo() {
